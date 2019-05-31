8deb8e14dff5e4b5a479c958c1c01453680f8889
silashuereca <silasgranthuereca@gmail.com>
2019-05-31 00:35:20 -0500

    edited php and html files

--- send_form_email.php
+++ send_form_email.php
@@ -26,9 +26,7 @@ if(isset($_POST['email'])) {
  
     // validation expected data exists
     if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
         !isset($_POST['comments'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
@@ -36,10 +34,7 @@ if(isset($_POST['email'])) {
      
  
     $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
     $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -54,13 +49,6 @@ if(isset($_POST['email'])) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
  
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
  
   if(strlen($error_message) > 0) {
     died($error_message);
@@ -91,7 +79,7 @@ $headers = 'From: '.$email_from."\r\n".
  
 <!-- include your own success html here -->
  
-Thank you for contacting us. We will be in touch with you very soon.
+Thank you for contacting me. I will get in touch with you as soon as possible.
  
 <?php
  
