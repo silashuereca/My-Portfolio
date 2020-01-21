1da62a36598bef6e307e238d4d6be7bbaa8f0432
silashuereca <silasgranthuereca@gmail.com>
2020-01-21 09:19:27 -0600

    added new skill to portfolio site

--- main.js
+++ main.js
@@ -24,3 +24,6 @@ TweenMax.to('.jquery', 1, { marginLeft: 0 });
 TweenMax.to('.react', 2, { marginLeft: 0 });
 TweenMax.to('.gsap', 3, { marginLeft: 0 });
 TweenMax.to('.git', 4, { marginLeft: 0 });
+
+// third row
+TweenMax.to('.vue', 1, { marginLeft: 0 });
