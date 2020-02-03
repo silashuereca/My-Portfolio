641bdceda47fa6a3dd71970eaca8d106a7b64a19
silashuereca <silasgranthuereca@gmail.com>
2020-02-02 22:38:19 -0600

    added new animation for portfolio

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 // Creating type effect
-let i = 0;
-let words = "Hello, my name is Silas Huereca. I'm a Front End Developer living in Oklahoma City, OK.";
-let speed = 40;
+// let i = 0;
+// let words = "Hello, my name is Silas Huereca. I'm a Front End Developer living in Oklahoma City, OK.";
+// let speed = 40;
 
-function typeWriter() {
-	if (i < words.length) {
-		document.getElementById('introText').innerHTML += words.charAt(i);
-		i++;
-		setTimeout(typeWriter, speed);
-	}
-}
+// function typeWriter() {
+// 	if (i < words.length) {
+// 		document.getElementById('introText').innerHTML += words.charAt(i);
+// 		i++;
+// 		setTimeout(typeWriter, speed);
+// 	}
+// }
 
 //animations
 
+let time = 2;
+
 //top row
-TweenMax.to('.html', 1, { marginLeft: 0 });
-TweenMax.to('.css', 2, { marginLeft: 0 });
-TweenMax.to('.sass', 3, { marginLeft: 0 });
-TweenMax.to('.javascript', 4, { marginLeft: 0 });
+TweenMax.to('.html', time, { marginTop: 0 });
+TweenMax.to('.css', time, { marginTop: 0 });
+TweenMax.to('.sass', time, { marginLeft: 0 });
+TweenMax.to('.javascript', time, { marginTop: 0 });
 
 //bottom row
-TweenMax.to('.jquery', 1, { marginLeft: 0 });
-TweenMax.to('.react', 2, { marginLeft: 0 });
-TweenMax.to('.gsap', 3, { marginLeft: 0 });
-TweenMax.to('.git', 4, { marginLeft: 0 });
+TweenMax.to('.jquery', time, { marginLeft: 0 });
+TweenMax.to('.react', time, { width: '100%' });
+TweenMax.to('.gsap', time, { marginTop: 0 });
+TweenMax.to('.git', time, { width: '100%' });
 
 // third row
-TweenMax.to('.vue', 1, { marginLeft: 0 });
-TweenMax.to('.bootstrap', 2, { marginLeft: 0 });
+TweenMax.to('.vue', time, { height: '100%' });
+TweenMax.to('.bootstrap', time, { marginLeft: 0 });
